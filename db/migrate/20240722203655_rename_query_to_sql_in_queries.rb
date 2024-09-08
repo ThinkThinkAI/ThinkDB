013142231cefccffad0c7aede49a55a6c16e883c
Test <test@example.com>
2024-09-08 14:33:36 -0700

    should follow inflections

--- db/migrate/20240722203655_rename_query_to_sql_in_queries.rb
+++ db/migrate/20240722203655_rename_query_to_sql_in_queries.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class RenameQueryToSqlInQueries < ActiveRecord::Migration[7.1]
+class RenameQueryToSQLInQueries < ActiveRecord::Migration[7.1]
   def change
     rename_column :queries, :query, :sql
   end
