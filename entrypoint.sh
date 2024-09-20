fedc8d65ff7280fdbe12812b6fcc4ae245573ba0
Test <test@example.com>
2024-09-19 21:58:38 -0700

    remove not needed entry points.. make sure credentials stay even after restarts

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,17 @@ if [ -z "$SECRET_KEY_BASE" ]; then
   export SECRET_KEY_BASE=$(bin/rails secret)
 fi
 
-echo "Running bundle exec rake assets:precompile"
+if [ -f storage/credentials.yml.enc ]; then
+  if [ ! -f config/credentials.yml.enc ]; then
+    cp storage/credentials.yml.enc config/credentials.yml.enc
+  fi
+else
+  EDITOR=sed rails credentials:edit --environment production
+  cp config/credentials.yml.enc storage/credentials.yml.enc
+fi
+
+
+echo "Remove PID"
 rm -f /app/tmp/pids/server.pid
 echo "Running bundle exec rake assets:precompile"
 bundle exec rake assets:precompile RAILS_ENV=production
@@ -15,7 +25,6 @@ echo "Running bundle exec rails db:migrate RAILS_ENV=production"
 bundle exec rails db:migrate RAILS_ENV=production
 
 # Start Sidekiq worker in the background
-./sidekiq-entrypoint.sh &
 bundle exec sidekiq &
 
 
